b53a67482c10ed5ddb1bed7810cf4a9216e16c45
Bob <SBob.com>
2024-07-09 11:51:56 +0800

    first commit

---
+++ do.sh
@@ -0,0 +1,161 @@
+#!/bin/sh
+git add 记录专题1/06-1/M06-01-000*
+git commit -m "first commit"
+git push -u origin main
+
+git add 记录专题1/06-1/M06-01-001*
+git commit -m "first commit"
+git push -u origin main
+
+git add 记录专题1/06-1/M06-01-002*
+git commit -m "first commit"
+git push -u origin main
+
+git add 记录专题1/06-1/M06-01-003*
+git commit -m "first commit"
+git push -u origin main
+
+git add 记录专题1/06-1/M06-01-004*
+git commit -m "first commit"
+git push -u origin main
+
+git add 记录专题1/06-3/M06-03-000*
+git commit -m "first commit"
+git push -u origin main
+
+git add 记录专题1/06-3/M06-03-001*
+git commit -m "first commit"
+git push -u origin main
+
+git add 记录专题1/06-3/M06-03-002*
+git commit -m "first commit"
+git push -u origin main
+
+git add 记录专题1/06-3/M06-03-003*
+git commit -m "first commit"
+git push -u origin main
+
+git add 记录专题1/06-3/*
+git commit -m "first commit"
+git push -u origin main
+
+git add 记录专题1/*
+git commit -m "first commit"
+git push -u origin main
+
+git add 记录专题2/06-4/M06-04-000*
+git commit -m "first commit"
+git push -u origin main
+
+git add 记录专题2/06-4/M06-04-001*
+git commit -m "first commit"
+git push -u origin main
+
+git add 记录专题2/06-4/M06-04-002*
+git commit -m "first commit"
+git push -u origin main
+
+git add 记录专题2/06-4/M06-04-003*
+git commit -m "first commit"
+git push -u origin main
+
+git add 记录专题2/06-4/M06-04-004*
+git commit -m "first commit"
+git push -u origin main
+
+git add 记录专题2/06-4/M06-04-005*
+git commit -m "first commit"
+git push -u origin main
+
+git add 记录专题2/06-5/M06-05-000*
+git commit -m "first commit"
+git push -u origin main
+
+git add 记录专题2/06-5/M06-05-001*
+git commit -m "first commit"
+git push -u origin main
+
+git add 记录专题2/06-5/M06-05-002*
+git commit -m "first commit"
+git push -u origin main
+
+git add 记录专题2/06-5/M06-05-003*
+git commit -m "first commit"
+git push -u origin main
+
+git add 记录专题2/06-5/M06-05-004*
+git commit -m "first commit"
+git push -u origin main
+
+git add 记录专题2/06-5/M06-05-005*
+git commit -m "first commit"
+git push -u origin main
+
+git add 记录专题2/06-5/M06-05-006*
+git commit -m "first commit"
+git push -u origin main
+
+git add 记录专题2/06-5/*
+git commit -m "first commit"
+git push -u origin main
+
+git add 记录专题3/06-6-1/M06-06-0006.zip.*
+git commit -m "first commit"
+git push -u origin main
+
+git add 记录专题3/06-6-1/M06-06-0007.zip.*
+git commit -m "first commit"
+git push -u origin main
+
+git add 记录专题3/06-6-1/M06-06-0008*
+git commit -m "first commit"
+git push -u origin main
+
+git add 记录专题3/06-6-1/M06-06-0009*
+git commit -m "first commit"
+git push -u origin main
+
+git add 记录专题3/06-6-1/M06-06-0010*
+git commit -m "first commit"
+git push -u origin main
+
+git add 记录专题3/06-6-1/M06-06-0011*
+git commit -m "first commit"
+git push -u origin main
+
+git add 记录专题3/06-6-1/M06-06-0012*
+git commit -m "first commit"
+git push -u origin main
+
+git add 记录专题3/06-6-1/M06-06-0013*
+git commit -m "first commit"
+git push -u origin main
+
+git add 记录专题3/06-6-1/M06-06-000*
+git commit -m "first commit"
+git push -u origin main
+
+git add 记录专题3/06-6-1/M06-06-001*
+git commit -m "first commit"
+git push -u origin main
+
+git add 记录专题3/06-6-1/M06-06-002*
+git commit -m "first commit"
+git push -u origin main
+
+git add 记录专题3/06-6-1/M06-06-003*
+git commit -m "first commit"
+git push -u origin main
+
+git add 记录专题3/06-6-1/M06-06-004*
+git commit -m "first commit"
+git push -u origin main
+
+git add 记录专题3/*
+git commit -m "first commit"
+git push -u origin main
+
+
+
+
+
